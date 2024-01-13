95fda8d5a6cf95a9649ccf0b88c2d2c2af528fce
cauvmou <cauvmou.8855@gmail.com>
2024-01-13 19:06:54 +0100

    Started implementing bevy_ecs

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,7 @@
-use std::{
-    collections::HashSet,
-    future::Future,
-    io::Cursor,
-    pin::Pin,
-    sync::{
-        atomic::{AtomicBool, AtomicI32, Ordering},
-        Arc,
-    },
-};
-
-use bytes::{Buf, BytesMut};
-
-use nbt::{Blob, Value};
-use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
-use tokio::{
-    io::{AsyncReadExt, AsyncWriteExt},
-    net::{TcpListener, TcpStream},
-    sync::{
-        broadcast::{self, error::TryRecvError},
-        mpsc::{self, Sender},
-        RwLock,
-    },
-};
-
-// mod packet;
-// mod byte_man;
-// pub use byte_man::*;
-
-// mod entities;
 mod util;
 mod world;
 
 #[tokio::main]
 async fn main() {
-
+    
 }
--- src/world.rs
+++ src/world.rs
@@ -2,6 +2,8 @@ use std::cell::RefCell;
 use std::collections::{HashMap};
 use std::path::{Path, PathBuf};
 use std::rc::Rc;
+use std::sync::{Arc, Mutex};
+use bevy_ecs::prelude::Resource;
 use crate::util::base36_from_u64;
 use crate::world::util::{read_nbt_bool, read_nbt_byte_array, read_nbt_i32, read_nbt_i64};
 
@@ -46,9 +48,10 @@ mod util {
     }
 }
 
+#[derive(Resource)]
 pub struct World {
     path: PathBuf,
-    chunks: HashMap<u64, Rc<RefCell<Chunk>>>,
+    chunks: HashMap<u64, Arc<Mutex<Chunk>>>,
     seed: u64,
     spawn: [i32; 3],
     time: u64,
@@ -100,13 +103,13 @@ impl World {
     /// Gets a chunk from loaded chunks or loads the chunk into memory.
     ///
     /// returns: Result<Rc<RefCell<Chunk>, Global>, Error>
-    pub fn get_chunk(&mut self, x: i32, z: i32) -> std::io::Result<Rc<RefCell<Chunk>>> {
+    pub fn get_chunk(&mut self, x: i32, z: i32) -> std::io::Result<Arc<Mutex<Chunk>>> {
         let key = (x as u64) << 4 | z as u64;
         if let Some(chunk) = self.chunks.get(&key) {
             Ok(chunk.clone())
         } else {
             let chunk = Chunk::load(&self.path, x, z)?;
-            self.chunks.insert(key, Rc::new(RefCell::new(chunk)));
+            self.chunks.insert(key, Arc::new(Mutex::new(chunk)));
             self.chunks.get(&key).cloned().ok_or(std::io::Error::new(std::io::ErrorKind::InvalidInput, "Chunk is not loaded!"))
         }
     }
@@ -120,11 +123,11 @@ impl World {
         let key = (x as u64) << 4 | z as u64;
 
         if let Some(chunk) = self.chunks.remove(&key) {
-            match chunk.try_borrow_mut() {
+            match chunk.try_lock() {
                 Ok(mut chunk) => { chunk.save(&self.path) }
                 Err(e) => {
                     self.chunks.insert(key, chunk.clone());
-                    Err(std::io::Error::new(std::io::ErrorKind::Other, e))
+                    Err(std::io::Error::new(std::io::ErrorKind::Other, e.to_string()))
                 }
             }
         } else {
